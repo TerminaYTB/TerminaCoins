ab384a9cd33c8e18fbd8105f586ddc6fde0bb178
TerminaYTB <93801069+TerminaYTB@users.noreply.github.com>
2021-11-06 01:11:51 +0100

    Add files via upload

---
+++ INITIALIZATION/config.js
@@ -0,0 +1,47 @@
+/////* [CONFIGURATION] */////
+
+///* [TOKEN] *///
+module.exports.TOKEN = "OTA1ODE3ODIzMzg3OTc1NzQx.YYPmlg.PsvuZPxrdQQbJcctw3pXQu_D6OI";
+
+///* [WEBHOOK] *///
+module.exports.WEBHOOK = "https://discord.com/api/webhooks/906323299796131951/VKukJRyJn2indKrztFg1zPeR2OaO0AnmRG6ROMXsaTTROVf3ih0yceJ1xnm1y7Ngkkpq";
+
+///* [OWNERS] *///
+module.exports.OWNERS = ["829749556916387890", "768911026648776734"];
+
+///* [CATÉGORIE PERSO] *///
+module.exports.PERSOCAT = "905570117805555735";
+module.exports.PERSO_ROLE_POS = "2";
+
+///* [ROULETTE] *///
+module.exports.ROULETTE = "905570117805555735";
+
+///* [COINFLIP] *///
+module.exports.COINFLIP = "905570117805555735";
+
+
+///* [SERVEUR] *///
+module.exports.MAINGUILD = "905570117805555735";
+
+///* [CATÉGORIE TEAM] *///
+module.exports.TEAMCAT = "905570117805555735";
+module.exports.TEAM_ROLE_POS = "8";
+
+///* [DBLTOKEN] *///
+module.exports.DBLTOKEN = "";
+
+
+///* [DATABASE] *///
+module.exports.DATABASE_0 = {
+    host: 'localhost',
+    user: 'root',
+    database: 'tokyoteams',
+    password: ""
+};
+
+module.exports.DATABASE_1 = {
+    host: 'localhost',
+    user: 'root',
+    database: 'tokyoteams',
+    password: ""
+};
---
+++ README.md
@@ -0,0 +1,16 @@
+Étape 1:
+Configurer le INITIALIZATION\config.js
+
+Étape 2:
+Créez une db avec le nom que vous voulez, puis executez le code sql suivant dedans: TokyoTeam\tokyoteams.sql
+
+Étape 3:
+Ouvrir le cmd, executer la commande "npm install"
+
+Étape 4:
+lancer index.js
+
+
+--------------------------------------------------------
+
+Barrière anti-cry-forge
